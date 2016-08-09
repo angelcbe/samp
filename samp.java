fffe554f91223c866db92bd176050ba4e0fd313c
angelcbe <angelin2803@gmail.com>
2016-08-09 16:21:32 +0530

    Create samp.java

---
+++ samp.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=7;
+if(a%2==0)
+{
+System.out.println("even");
+}
+else
+System.out.println("odd");
+	}
+}
